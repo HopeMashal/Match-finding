0f96930eed4338cae0275a38be657b85a4e016a8
HopeMashal <mashal.hope@gmail.com>
2021-12-23 23:43:30 +0200

    Edit the react

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
 import './App.css';
 import React from 'react';
 import ApprovalCard from './components/ApprovalCard';
-import {Data} from './components/imageData';
+import { Data } from './components/imageData';
 
 class App extends React.Component {
-  state={countLike:0,countDislike:0}
+  state={
+    countLike:0,
+    countDislike:0,
+    numOfItems: Data.length-1,
+    currentItem: 0,
+    isEnd: false,
+  }
+
+  setLike = () => {
+    this.setState((prevState) => {
+      return { countLike: prevState.countLike + 1 };
+    });
+  };
+
+  setDislike = () => {
+    this.setState((prevState) => {
+      return { countDislike: prevState.countDislike + 1 };
+    });
+  };
+
+  setNext = () => {
+    if (this.state.currentItem + 1 <= this.state.numOfItems) {
+      this.setState((prevState) => {
+        return { currentItem: prevState.currentItem + 1 };
+      });
+    } else {
+      this.setState({ isEnd: true });
+    }
+  };
+
   render() {
     return (
       <div className="App">
         <ApprovalCard 
           likeNum={this.state.countLike}
           dislikeNum={this.state.countDislike}
+          likeFun={this.setLike}
+          dislikeFun={this.setDislike}
+          nextItem={this.setNext}
+          isEnd={this.state.isEnd}
+          currentItem={this.state.currentItem}
         >
-          <p>{Data[0].description}</p><hr/>
-          <div className="img-container">
-            <img className="animal-img" alt="myimage" src={Data[0].url}></img>
-          </div>
         </ApprovalCard>
       </div>
     );
--- src/components/ApprovalCard.js
+++ src/components/ApprovalCard.js
@@ -3,19 +3,41 @@ import Right from './images/true.png';
 import Worng from './images/false.png';
 import Like from './images/like.png';
 import Dislike from './images/dislike.png';
+import {Data} from './imageData'
 
-const ApprovalCard = (props) =>{
+const ApprovalCard = ({nextItem,likeFun,dislikeFun,isEnd,dislikeNum,likeNum,currentItem}) =>{
+  const TrueClick = () => {
+    if (isEnd) return;
+    likeFun();
+    nextItem();
+  };
+  const FalseClick = () => {
+    if (isEnd) return;
+    dislikeFun();
+    nextItem();
+  };
+  if(isEnd){
+    const mycontainer=document.querySelector('.image-description');
+    if(likeNum===dislikeNum) mycontainer.innerHTML=`<p><b>The number of likes equals the number of dislikes<br/>You should be more cheerful!!<b/><p/>`;
+    if(likeNum>dislikeNum) mycontainer.innerHTML=`<p><b>The number of likes is more than the number of dislikes<br/>Great, you are really cheerful :)<b/><p/>`;
+    if(likeNum<dislikeNum) mycontainer.innerHTML=`<p><b>The number of likes is less than the number of dislikes<br/>Your condition is hopeless!!<b/><p/>`;
+  }
   return(
     <div className="ui card">
       <div className='extra content'>
-        <div className='nav-green'><img src={Like} alt="like"/> {props.likeNum}</div>
-        <div className='nav-red'>{props.dislikeNum} <img src={Dislike} alt="dislike"/></div>
+        <div className='nav-green'><img src={Like} alt="like"/> {likeNum}</div>
+        <div className='nav-red'>{dislikeNum} <img src={Dislike} alt="dislike"/></div>
       </div>
-      <div className="content">{props.children}</div>
+      <div className="content image-description">
+          <p>{Data[currentItem].description}</p><hr/>
+          <div className="img-container">
+            <img className="animal-img" alt="myimage" src={Data[currentItem].url}></img>
+          </div>
+      </div>
       <div className='extra content'>
         <div className='ui two buttons'>
-          <div className='ui basic green button'><img className='YNBtn' alt="right" src={Right}/></div>
-          <div className='ui basic red button'><img className='YNBtn' alt="worng" src={Worng}/></div>
+          <div className='ui basic green button' onClick={TrueClick}><img className='YNBtn' alt="right" src={Right}/></div>
+          <div className='ui basic red button' onClick={FalseClick}><img className='YNBtn' alt="worng" src={Worng}/></div> 
         </div>
       </div>
     </div>
